f3e85dc55e1f0dd09cc84c7cc771e4fd1e6d26a5
despair <>
2018-11-05 06:05:05 -0500

    Bidirectional influence model

--- linker.py
+++ linker.py
@@ -11,27 +11,26 @@ from networkx.readwrite import json_graph
 parser = argparse.ArgumentParser(description='Analyze Wikipedia links.')
 parser.add_argument('page_links', type=argparse.FileType('r'),
                     help='CSV database of page links')
-parser.add_argument('pages_from', type=argparse.FileType('r'),
-                    help='list of articles from which to find links (PetScan JSON)')
-parser.add_argument('pages_to', type=argparse.FileType('r'),
-                    help='list of articles to which to find links (PetScan JSON)')
+parser.add_argument('A', type=argparse.FileType('r'),
+                    help='list of articles for group A (PetScan JSON)')
+parser.add_argument('B', type=argparse.FileType('r'),
+                    help='list of articles for group B (PetScan JSON)')
 parser.add_argument('output', type=argparse.FileType('w'),
                     help='output file')
-parser.add_argument('--reverse', action='store_true',
-                    help='reverse edge direction in output graph')
 args = parser.parse_args()
 
 
 class PageSet:
+    title_by_id = dict()
+
     def __init__(self, data):
         self.ids = set()
         self.titles = set()
-        self.title_by_id = dict()
 
         for page in data['*'][0]['a']['*']:
             self.ids.add(page['id'])
             self.titles.add(page['title'])
-            self.title_by_id[page['id']] = page['title']
+            PageSet.title_by_id[page['id']] = page['title']
 
     def __contains__(self, item):
         if isinstance(item, int):
@@ -39,14 +38,15 @@ class PageSet:
         else:
             return item in self.titles
 
-    def get_title(self, id):
-        return self.title_by_id[id]
+    @staticmethod
+    def get_title(id):
+        return PageSet.title_by_id[id]
 
 
-pages_from = PageSet(json.load(args.pages_from))
-pages_to = PageSet(json.load(args.pages_to))
+A = PageSet(json.load(args.A))
+B = PageSet(json.load(args.B))
 
-g = nx.DiGraph()
+g = nx.Graph()
 
 # search page links
 reader = csv.reader(args.page_links)
@@ -57,21 +57,22 @@ for row in reader:
     pl_namespace = row[3]
 
     # ignore links outside default namespace
+    # search for links A -> B or B -> A
     if pl_from_namespace is '0' and pl_namespace is '0' and \
-       pl_from in pages_from and pl_title in pages_to:
-        g.add_edge(pages_from.get_title(pl_from), pl_title)
+       (pl_from in A and pl_title in B or pl_from in B and pl_title in A):
+        g.add_edge(PageSet.get_title(pl_from), pl_title)
 
 # add group information to nodes
 for node in g.nodes:
-    group = (node in pages_from) * 2 + (node in pages_to) * 1
+    group = (node in B) * 2 + (node in A) * 1
+    # node in A:    1
+    # node in B:    2
+    # node in both: 3
     g.nodes[node]['group'] = group
-
-# reverse direction of edges if requested
-if args.reverse:
-    g = g.reverse(copy=False)
 
 # use numeric indices
 g = nx.convert_node_labels_to_integers(g, label_attribute='id')
 g_json = json_graph.node_link_data(g, attrs={'name': 'index'})
 
+# write JSON to output
 json.dump(g_json, args.output, separators=(',', ':'))
