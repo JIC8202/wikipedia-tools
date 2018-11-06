25f9df526f51f41d80ad94a134018bc4cc90ba92
despair <>
2018-11-06 01:00:19 -0500

    [testing] 'hairball' model
    find links between any people in the input sets

--- linker.py
+++ linker.py
@@ -21,16 +21,16 @@ args = parser.parse_args()
 
 
 class PageSet:
-    title_by_id = dict()
-
-    def __init__(self, data):
+    def __init__(self, data=None):
         self.ids = set()
         self.titles = set()
+        self.title_by_id = dict()
 
-        for page in data['*'][0]['a']['*']:
-            self.ids.add(page['id'])
-            self.titles.add(page['title'])
-            PageSet.title_by_id[page['id']] = page['title']
+        if data:
+            for page in data['*'][0]['a']['*']:
+                self.ids.add(page['id'])
+                self.titles.add(page['title'])
+                self.title_by_id[page['id']] = page['title']
 
     def __contains__(self, item):
         if isinstance(item, int):
@@ -38,13 +38,19 @@ class PageSet:
         else:
             return item in self.titles
 
-    @staticmethod
-    def get_title(id):
-        return PageSet.title_by_id[id]
+    def get_title(self, id):
+        return self.title_by_id[id]
 
+    def union(self, B):
+        C = PageSet()
+        C.ids = self.ids | B.ids
+        C.titles = self.titles | B.titles
+        C.title_by_id = {**self.title_by_id, **B.title_by_id}
+        return C
 
 A = PageSet(json.load(args.A))
 B = PageSet(json.load(args.B))
+All = A.union(B)
 
 g = nx.DiGraph()
 
@@ -57,10 +63,9 @@ for row in reader:
     pl_namespace = row[3]
 
     # ignore links outside default namespace
-    # search for links A -> B or B -> A
     if pl_from_namespace is '0' and pl_namespace is '0' and \
-       (pl_from in A and pl_title in B or pl_from in B and pl_title in A):
-        g.add_edge(PageSet.get_title(pl_from), pl_title)
+       pl_from in All and pl_title in All:
+        g.add_edge(All.get_title(pl_from), pl_title)
 
 # add group information to nodes
 for node in g.nodes:
@@ -76,3 +81,4 @@ g_json = json_graph.node_link_data(g, attrs={'name': 'index'})
 
 # write JSON to output
 json.dump(g_json, args.output, separators=(',', ':'))
+nx.write_gexf(g, "test.gexf")
